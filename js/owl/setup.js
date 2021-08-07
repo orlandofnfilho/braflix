2a4c5563a85733836a5581361f803d8b64b68a55
Orlando Ferreria <orlandolilo@Hotmail.com>
2021-08-07 17:37:12 -0300

    Update

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -15,3 +15,33 @@ $('.owl-carousel').owlCarousel({
     }
 })
 
+//Modal
+var modal = document.getElementById("myModal");
+
+
+var btn = document.getElementById("myBtn");
+
+
+var span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+//Assistir
+function assistir() {
+    window.open("https://www.youtube.com/embed/z8fOvojcpMc");
+  }
